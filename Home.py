39da8b128f25ca462fc20adef4cfbfb4b77cdb3c
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 20:30:19 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,15 @@ import streamlit as st
 
 # Include custom CSS for Roboto font from Google Fonts
 font_url = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-st.markdown(f"<style>@import url('{font_url}'); body {{ font-family: 'Roboto', sans-serif; }}</style>", unsafe_allow_html=True)
+css = f"""
+<style>
+@import url('{font_url}');
+html, body, [class*="css"] {{
+    font-family: 'Roboto', sans-serif !important;
+}}
+</style>
+"""
+st.markdown(css, unsafe_allow_html=True)
 
 st.title("🌾 MK316 - Language Learning Applications")
 
@@ -11,6 +19,4 @@ st.markdown("""
 Welcome to MK316 - your hub for exploring innovative language learning applications powered by Streamlit! Dive into our projects, or explore my digital classroom at [mrkim21.github.io](https://mrkim21.github.io).
 """)
 
-st.markdown("""
-Since Oct. 14, 2024
-""")
+st.markdown("Since Oct. 14, 2024")
