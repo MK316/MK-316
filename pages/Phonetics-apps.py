bae728af6c651b2f761dcd5f352cf16cd9e59139
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:45:06 +0900

    Update Phonetics-apps.py

--- pages/Phonetics-apps.py
+++ pages/Phonetics-apps.py
@@ -53,4 +53,9 @@ def phonetics_apps_page():
             st.markdown("🌀[App link](https://mk-316-mp3towav.hf.space/): Convert mp3 to wav file", unsafe_allow_html=True)
             st.markdown("2024.10.15")
 
+# URL to the raw image on GitHub
+image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
+# Display the image
+st.image(image_url, caption="\"He who knows no foreign languages knows nothing of his own.\" — Johann Wolfgang von Goethe", use_column_width=True)
+
 phonetics_apps_page()
