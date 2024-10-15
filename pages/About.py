94be041445939d4e89a5663b42fc90419949c824
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:53:26 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -6,3 +6,8 @@ st.markdown("\n\n")
 st.markdown("- Github ID: MK316")
 st.markdown("- Affiliation: Dept. of English Education, GNU ⛺ [Goto](https://englishedu.gnu.ac.kr)")
 st.markdown("- My digital classroom: [mrkim21.github.io](https://mrkim21.github.io)")
+
+# URL to the raw image on GitHub
+image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
+# Display the image
+st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_column_width=True)
