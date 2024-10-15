dd09735fb1fd86254569d8718a774d2eb2fae2ae
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 23:00:38 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.markdown("### _Application Gallery_")
 
 # Additional content
 st.markdown("""
-Welcome to MK316 - your hub for exploring innovative language learning applications powered by Streamlit! Dive into our projects, or explore my digital classroom at [mrkim21.github.io](https://mrkim21.github.io).
+Bonjour! This will be my digital app gallery for exploring innovative language learning applications powered by Streamlit! (Started on Oct. 14) The gallery is still under preparation, but in the meantime, feel free to explore my digital classroom at [mrkim21.github.io](https://mrkim21.github.io).
 """)
 
 st.markdown("""
