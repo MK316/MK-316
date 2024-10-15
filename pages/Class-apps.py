13edfa7bedcd793d7281f48495d8968f224f8b05
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 01:59:08 +0900

    Update Class-apps.py

--- pages/Class-apps.py
+++ pages/Class-apps.py
@@ -1,3 +1,57 @@
-import streamlit as sr
+import streamlit as st
 
-sr.write("I will introduce and share applications designed to enhance classroom management.")
+def phonetics_apps_page():
+    st.title('Classroom management')
+    st.write('Applications used in my classroom.')
+
+    # Describing your apps briefly
+    st.markdown("""
+    This page features a collection of simple applications tailored for class management. These tools are designed to streamline classroom processes, enhance organization, and facilitate smooth communication between educators and students. By integrating these applications, educators can efficiently manage their classes and improve overall classroom dynamics.
+    """)
+
+
+    # First row with three columns
+    col1, col2, col3 = st.columns(3)  # Define columns for the first row
+
+    with col1:
+        st.image("images/button01.png", width=100)
+        if st.button('App 1: QR Code', key='1'):
+            st.markdown("🌀[App link](https://mk-316-qrcode.hf.space/): QR code generator", unsafe_allow_html=True)
+            st.markdown("2024.10.14")
+    with col2:
+        st.image("images/button01.png", width=100)
+        if st.button('App 2: Stopwatch', key='2'):
+            st.markdown("🌀[App link](https://mk-316-Stopwatch.hf.space/): Stopwatch with current time", unsafe_allow_html=True)
+            st.markdown("2024.10.14")
+    
+    with col3:
+        st.image("images/button01.png", width=100)
+        if st.button('App 3: Grouping', key='3'):
+            st.markdown("🌀[App link](https://mk-316-grouping.hf.space/): Group students for activities; roster", unsafe_allow_html=True)
+            st.markdown("2024.1.24")
+    
+    # Add some space before the second row
+    st.write("\n\n")
+
+    # Second row with three columns
+    col4, col5, col6 = st.columns(3)  # Correct typo here
+
+    with col4:
+        st.image("images/button03.png", width=100)
+        if st.button('App 4: Recorder', key='4'):
+            st.markdown("🌀[App link](https://mk-316-recorder.hf.space/): Recording from your device", unsafe_allow_html=True)
+            st.markdown("2024.02.28")
+
+    with col5:
+        st.image("images/button03.png", width=100)
+        if st.button('App 5: TTS app', key='5'):
+            st.markdown("🌀[App link](https://mk-316-ttsapp.hf.space/): Basic Text-to-Speech", unsafe_allow_html=True)
+            st.markdown("2024.1.20")
+
+    with col6:
+        st.image("images/button01.png", width=100)
+        if st.button('App 6: Wordcloud', key='6'):
+            st.markdown("🌀[App link](https://mk-316-wordcloud.hf.space/): Generate a simple wordcloud image from texts", unsafe_allow_html=True)
+            st.markdown("2024.04.14")
+
+phonetics_apps_page()
