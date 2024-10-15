8814da493c9881a254c13ee8d53eabb9cfd7ad7e
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 02:36:23 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,7 +1,8 @@
 import streamlit as sr
 
 sr.markdown("### ☕ About me...")
-
+sr.markdown(![](/images/profile-small.jpg)
+sr.markdown("\n\n")
 sr.markdown("- Github ID: MK316")
 sr.markdown("- Affiliation: Dept. of English Education, GNU ⛺ [Goto](https://englishedu.gnu.ac.kr)") 
 sr.markdown("- My digital classroom: [mrkim21.github.io](https://mrkim21.github.io)")
