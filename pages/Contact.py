56220cc7746ef69d348d3ec8a2d473b8050bef6f
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 21:48:29 +0900

    Create Contact.py

---
+++ pages/Contact.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Contact Me")
+
+st.markdown("""
+Feel free to get in touch with me by visiting my Padlet page.  
+[Click here to contact me via Padlet](https://padlet.com/mirankim316/message
+""")
