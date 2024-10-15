083ed5d45593848ecb4dc8a4b1eef4353b98de8a
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 00:38:06 +0900

    Update Phonetics-apps.py

--- pages/Phonetics-apps.py
+++ pages/Phonetics-apps.py
@@ -14,7 +14,7 @@ def phonetics_apps_page():
 
     with col1:
         st.image("images/ipa01.png", width=100)
-        if st.button('App 1: \nIPA quiz', key='1'):
+        if st.button('App 1: IPA-quiz', key='1'):
             st.markdown("[Open the app](https://ipa-practice.streamlit.app/) \n IPA symbols, Quiz, Phonetic description", unsafe_allow_html=True)
     with col2:
         st.image("images/ipa01.png", width=100)
@@ -26,14 +26,4 @@ def phonetics_apps_page():
         if st.button('App 3: Playsound', key='3'):
             st.markdown("Click here to launch [App 3](https://playsound.streamlit.app/)", unsafe_allow_html=True)
             
-    with col4:
-        st.image("images/ipa01.png", width=100)
-        if st.button('Launch App 4', key='4'):
-            st.markdown("Click here to launch [App 2](https://MK-316-Stopwatch.hf.space)", unsafe_allow_html=True)
-    
-    with col5:
-        st.image("images/ipa01.png", width=100)
-        if st.button('Launch App 5', key='5'):
-            st.markdown("Click here to launch [App 3](https://playsound.streamlit.app/)", unsafe_allow_html=True)
-
 phonetics_apps_page()
