20d098c2e79a80ab8d0212cc050911db3e3d0f17
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:54:34 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -10,4 +10,4 @@ st.markdown("- My digital classroom: [mrkim21.github.io](https://mrkim21.github.
 # URL to the raw image on GitHub
 image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
 # Display the image
-st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_column_width=True)
+st.image(image_url, caption="\"Be curious, not judgmental.\" — Walt Whitman", use_column_width=True)
