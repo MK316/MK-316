76d3a7da5e2f63a7a4288ea9c4c21d121b17cb7d
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:05:26 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,9 +1,4 @@
 import streamlit as st  # Corrected the import alias to 'st' from 'sr'
-# URL to the raw image on GitHub
-image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
-# Display the image
-st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_column_width=True)
-
 
 st.markdown("### About me...")
 st.markdown("![Profile Image](https://github.com/MK316/MK-316/raw/main/images/profile-small.jpg)")  # Correct markdown syntax for images
