c988558ca368cd76bd1994ee15d8a8c9a2c3de76
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 01:47:23 +0900

    Update Phonetics-apps.py

--- pages/Phonetics-apps.py
+++ pages/Phonetics-apps.py
@@ -16,16 +16,18 @@ def phonetics_apps_page():
         st.image("images/button01.png", width=100)
         if st.button('App 1: \nIPA quiz', key='1'):
             st.markdown("🌀[App link](https://ipa-practice.streamlit.app/): IPA Quiz with phonetic descriptions; Scoring", unsafe_allow_html=True)
-    
+            st.markdown("2024.10.14")
     with col2:
         st.image("images/button03.png", width=100)
         if st.button('App 2: Playsound', key='2'):
             st.markdown("🌀[App link](https://playsound.streamlit.app/): Upload audio to play; Speed controller", unsafe_allow_html=True)
+            st.markdown("2024.10.14")
     
     with col3:
         st.image("images/button01.png", width=100)
         if st.button('App 3: Distinctive features', key='3'):
             st.markdown("🌀[App link](https://mk-316-featureapp01.hf.space/): Phonology, Sound grouping from distinctive features", unsafe_allow_html=True)
+            st.markdown("2024.10.15")
     
     # Add some space before the second row
     st.write("\n\n")
@@ -37,15 +39,18 @@ def phonetics_apps_page():
         st.image("images/button01.png", width=100)
         if st.button('App 4: List sounds', key='4'):
             st.markdown("🌀[App link](https://mk-316-ipaselect.hf.space/): Display sounds from phonetic descriptions", unsafe_allow_html=True)
+            st.markdown("2024.10.14")
 
     with col5:
         st.image("images/button01.png", width=100)
         if st.button('App 5: Feature Quiz', key='5'):
             st.markdown("🌀[App link](https://mk-316-feature-practice.hf.space/): Phonology, Distinctive feature quiz", unsafe_allow_html=True)
+            st.markdown("2024.10.14")
 
     with col6:
         st.image("images/button03.png", width=100)
         if st.button('App 6: MP3-to-wav', key='6'):
             st.markdown("🌀[App link](https://mk-316-mp3towav.hf.space/): Convert mp3 to wav file", unsafe_allow_html=True)
+            st.markdown("2024.10.15")
 
 phonetics_apps_page()
