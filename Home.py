0c867065d94a794f97ed647231e86cb8449699ce
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 20:26:25 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
+# Include custom CSS
+font_url = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+st.markdown(f"<style>@import url('{font_url}'); body {{ font-family: 'Roboto', sans-serif; }}</style>", unsafe_allow_html=True)
+
 st.title("🌾 MK316 - Language Learning Applications")
 
+# Additional content
 st.markdown("""
 Welcome to MK316 - your hub for exploring innovative language learning applications powered by Streamlit! Dive into our projects, or explore my digital classroom at [mrkim21.github.io](https://mrkim21.github.io).
+""")
+
+
+st.markdown("""
+Since Oct.14, 2024
 """)
