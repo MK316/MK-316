46c21836f70958a0f7590f8f0e7d173d68ae1252
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 20:06:45 +0900

    Update Class-apps.py

--- pages/Class-apps.py
+++ pages/Class-apps.py
@@ -1,3 +1,3 @@
 import streamlit as sr
 
-sr.write("I will introduce and share applications designed to enhance classroom management.)
+sr.write("I will introduce and share applications designed to enhance classroom management.")
