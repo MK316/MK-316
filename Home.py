37ce5bc8fe38509f9f2ab2de46895210eab33867
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:01:41 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,3 +14,8 @@ rather than relying on solutions that don't necessarily fit my students and clas
 My goal is to create **_simple_** but **_interactive_** tools that make language learning more engaging and effective. I'm excited to share this process with you as we explore new, impactful ways to teach languages.
 """)
 st.markdown("Since Oct. 14, 2024")
+
+# URL to the raw image on GitHub
+image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
+# Display the image
+st.image(image_url, caption="\"Wannna really learn coding? Give teaching a go!\"", use_column_width=True)
