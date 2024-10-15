36fc823d37229f1a15b004c6ccb5b1309c2a2a7e
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 00:33:54 +0900

    Update Phonetics-apps.py

--- pages/Phonetics-apps.py
+++ pages/Phonetics-apps.py
@@ -13,8 +13,7 @@ def phonetics_apps_page():
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
-        st.image("images/ipa01.png", width=100)
-        if st.button('Description', key='1'):
+        if st.button('App1: IPA quiz', key='1'):
             st.markdown("[Open the app](https://ipa-practice.streamlit.app/) \n IPA symbols, Quiz, Phonetic description", unsafe_allow_html=True)
     with col2:
         st.image("images/ipa01.png", width=100)
