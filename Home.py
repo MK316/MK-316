325d5b3a3eeceee46ab39920937f7e74ca4a85f2
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 02:52:39 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
 # Display the image
 st.image(image_url, caption="\"Wannna really learn coding? Give teaching a go!\"", use_column_width=True)
 
-st.title("MK316")
+st.markdown("![MK316](https://github.com/MK316/MK-316/raw/main/images/mk316.png")
 st.markdown("### _Welcome to my application gallery_")
 
 # Additional content
