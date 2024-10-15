8fbe54ab49fa605c4bd2e84ffd5610ef0334e310
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:41:38 +0900

    Update TCExam.py

--- pages/TCExam.py
+++ pages/TCExam.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def tce_apps_page():
-    st.title('Teacher Certificate Exam Apps')
+    st.title('TCE Prep')
     st.write('Applications for TCE preparing students.')
 
     # Describing your apps briefly
