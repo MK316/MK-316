19b56f5136aed91db0f0d6939c7e8ff370f60bbb
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 03:05:55 +0900

    Update English-learning.py

--- pages/English-learning.py
+++ pages/English-learning.py
@@ -1,3 +1,8 @@
 import streamlit as sr
+# URL to the raw image on GitHub
+image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
+# Display the image
+st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_column_width=True)
+
 
 sr.write("English learning applications (Under construction)")
