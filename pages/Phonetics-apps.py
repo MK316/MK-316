de5a27d427a3e40e89c3f7b4ceae559dbee88075
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 00:23:35 +0900

    Update Phonetics-apps.py

--- pages/Phonetics-apps.py
+++ pages/Phonetics-apps.py
@@ -26,5 +26,14 @@ def phonetics_apps_page():
         st.image("images/ipa01.png", width=100)
         if st.button('Launch App 3', key='3'):
             st.markdown("Click here to launch [App 3](https://playsound.streamlit.app/)", unsafe_allow_html=True)
+    with col4:
+        st.image("images/ipa01.png", width=100)
+        if st.button('Launch App 4', key='4'):
+            st.markdown("Click here to launch [App 2](https://MK-316-Stopwatch.hf.space)", unsafe_allow_html=True)
+    
+    with col5:
+        st.image("images/ipa01.png", width=100)
+        if st.button('Launch App 5', key='5'):
+            st.markdown("Click here to launch [App 3](https://playsound.streamlit.app/)", unsafe_allow_html=True)
 
 phonetics_apps_page()
