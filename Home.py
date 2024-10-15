8bdc2210abfe0c58a18cae5782d93e2e6f122db7
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 20:08:01 +0900

    Update and rename main.py to Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 MK316 - Language Learning Applications")
+st.title("⛄ MK316 - Language Learning Applications")
 st.write(
     "Let's start building using Streamlit! \nMeanwhile, you may want to see my previous digital classroom; head over to [mrkim21.github.io](https://mrkim21.github.io/)."
 )
