62c9672a78d6e3c87f621eaa3a6d2a4fd5e0c1ac
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 21:54:35 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -3,6 +3,5 @@ import streamlit as sr
 sr.markdown("### ☕ About me...")
 
 sr.markdown("- Github ID: MK316")
-sr.markdown("- Contact E-mail: 📧 ")
 sr.markdown("- Affiliation: Dept. of English Education, GNU ⛺ [Goto](https://englishedu.gnu.ac.kr)") 
 sr.markdown("- My digital classroom: [mrkim21.github.io](https://mrkim21.github.io)")
