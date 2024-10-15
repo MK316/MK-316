2ade9673dfbf0fae63d30775b89865812b1624a9
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 20:50:50 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,6 +23,6 @@ Welcome to MK316 - your hub for exploring innovative language learning applicati
 st.markdown("""
 I'm on a journey to learn coding to develop language apps that are **customized** and **learner-centered**, 
 rather than relying on solutions that don't necessarily fit my students and classroom. 
-My goal is to create simple but interactive tools that make language learning more engaging and effective. I'm excited to share this process with you as we explore new, impactful ways to teach languages.
+My goal is to create **_simple_** but **_interactive_** tools that make language learning more engaging and effective. I'm excited to share this process with you as we explore new, impactful ways to teach languages.
 """)
 st.markdown("Since Oct. 14, 2024")
