d5cdabd9ac89dda67802f7748ce7d78fa7dc3a95
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 02:26:35 +0900

    Create TCExam.py

---
+++ pages/TCExam.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def tce_apps_page():
+    st.title('Teacher Certificate Exam Apps')
+    st.write('Applications for TCE preparing students.')
+
+    # Describing your apps briefly
+    st.markdown("""
+    This page features applications designed to prepare users for Teacher Certificate Exams. The tools offer resources to help their prepration.
+    """)
+
+    # First row with three columns
+    col1 = st.columns(1)  # Define columns for the first row
+
+    with col1:
+        st.image("images/button01.png", width=100)
+        if st.button('App 1: TCE exam search', key='1'):
+            st.markdown("🌀[App link](https://mk316-TCE.hf.space/): Searcheable exam questions in phonetics & phonology", unsafe_allow_html=True)
+            st.markdown("2024.02.05")
+
+tce_apps_page()
