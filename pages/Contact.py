de13bf044fdbc26098f0541d941c5ecbaa0369cc
MK <99416359+MK316@users.noreply.github.com>
2024-10-15 21:20:48 +0900

    Update Contact.py

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,13 +1,24 @@
-import streamlit as st
+import streamlit as st  # pip install streamlit
 
-st.header(":mailbox: Get in touch with me!")
+st.header(":mailbox: Get In Touch With Me!")
+
 
 contact_form = """
 <form action="https://formsubmit.co/76a541470c923240c7e622e9f798cafa" method="POST">
-     <input type="text" name="name" required>
-     <input type="email" name="email" required>
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
      <button type="submit">Send</button>
 </form>
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
