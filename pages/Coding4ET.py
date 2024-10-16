bd448d44e758a5b661672d421c7fdcdf996535c4
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 13:52:58 +0900

    Update Coding4ET.py

--- pages/Coding4ET.py
+++ pages/Coding4ET.py
@@ -3,32 +3,48 @@ import streamlit as st
 def main():
     st.title('Coding4ET Tutorials')
 
-    # Layout: two columns for buttons
-    col1, col2 = st.columns(2)
-
-    # Defining button labels and corresponding URLs
+    # Define video URLs
     tutorial_links = {
-        'Lesson 3.1': 'https://youtu.be/uigxMFBR0Wg',
-        'Lesson 3.2': 'https://youtu.be/gPIe1Tgie1Q',
-        'Lesson 3.3': 'https://youtu.be/ofhRZ6k3Ryw',
-        'Lesson 3.4': 'https://youtu.be/vSsClackic4'
+        'Lesson 3.1': 'https://www.youtube.com/embed/link_for_lesson_3_1',
+        'Lesson 3.2': 'https://www.youtube.com/embed/link_for_lesson_3_2',
+        'Lesson 3.3': 'https://www.youtube.com/embed/link_for_lesson_3_3',
+        'Lesson 3.4': 'https://www.youtube.com/embed/link_for_lesson_3_4'
     }
 
-    # Placing buttons in columns
-    tutorials_in_col1 = ['Lesson 3.1', 'Lesson 3.2']
-    tutorials_in_col2 = ['Lesson 3.3', 'Lesson 3.4']
+    # Placeholder for the video iframe
+    video_container = st.empty()
+    default_video_url = tutorial_links['Lesson 3.1']  # Default video to display
+    video_container.html(
+        f"<iframe width='560' height='315' src='{default_video_url}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+        height=315
+    )
 
+    # Two columns for buttons
+    col1, col2 = st.columns(2)
+
+    # Button setup in columns
     with col1:
-        for lesson in tutorials_in_col1:
-            if st.button(lesson):
-                video_url = tutorial_links[lesson]
-                st.video(video_url)
-
+        if st.button('Lesson 3.1'):
+            video_container.html(
+                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.1']}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+                height=315
+            )
+        if st.button('Lesson 3.2'):
+            video_container.html(
+                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.2']}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+                height=315
+            )
     with col2:
-        for lesson in tutorials_in_col2:
-            if st.button(lesson):
-                video_url = tutorial_links[lesson]
-                st.video(video_url)
+        if st.button('Lesson 3.3'):
+            video_container.html(
+                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.3']}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+                height=315
+            )
+        if st.button('Lesson 3.4'):
+            video_container.html(
+                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.4']}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>",
+                height=315
+            )
 
 if __name__ == "__main__":
     main()
