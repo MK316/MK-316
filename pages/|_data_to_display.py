c886cd6df9f1497d34bd301e75ae2fb5f02be992
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 12:43:14 +0900

    Update |_data_to_display.py

--- pages/|_data_to_display.py
+++ pages/|_data_to_display.py
@@ -3,9 +3,6 @@ import numpy as np
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-# Ask for user input
-user_input = st.text_input("Enter text to create a word cloud:")
-
 # Creating the word cloud
 def create_wordcloud(text):
     wordcloud = WordCloud(width=800, height=400, background_color ='white').generate(text)
@@ -26,13 +23,14 @@ tab2.subheader("A tab with the data")
 tab2.write(data)
 
 # Word cloud tab
+tab3.subheader("A tab with a word cloud")
+tab3.write("Please enter some text to generate a word cloud.")
+# Move the user input here to appear below the instruction
+user_input = tab3.text_input("Enter text to create a word cloud:")
+
 if user_input:  # Check if there is user input before generating the word cloud
-    tab3.subheader("A tab with a word cloud")
     wordcloud = create_wordcloud(user_input)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis("off")
     tab3.pyplot(fig)
-else:
-    tab3.subheader("A tab with a word cloud")
-    tab3.write("Please enter some text to generate a word cloud.")
