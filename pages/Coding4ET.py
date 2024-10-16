23d38ce0111bcebec15a59c5d58c832bc6aaa876
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 14:07:28 +0900

    Update Coding4ET.py

--- pages/Coding4ET.py
+++ pages/Coding4ET.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 def main():
     st.title('Coding4ET Tutorials')
 
-    # Define video URLs (Use the "embed" link from YouTube)
+    # Define video URLs
     tutorial_links = {
         'Lesson 3.1': 'https://www.youtube.com/embed/link_for_lesson_3_1',
         'Lesson 3.2': 'https://www.youtube.com/embed/link_for_lesson_3_2',
@@ -12,10 +12,12 @@ def main():
         'Lesson 3.4': 'https://www.youtube.com/embed/link_for_lesson_3_4'
     }
 
-    # Placeholder for the video iframe
-    video_container = st.empty()
+    # Create a placeholder for video
+    video_placeholder = st.empty()
     default_video_url = tutorial_links['Lesson 3.1']  # Default video to display
-    components.html(
+
+    # Initial display
+    video_placeholder.html(
         f"<iframe width='560' height='315' src='{default_video_url}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
         height=315
     )
@@ -23,29 +25,24 @@ def main():
     # Two columns for buttons
     col1, col2 = st.columns(2)
 
-    # Button setup in columns
+    # Setup buttons to update the video iframe
     with col1:
         if st.button('Lesson 3.1'):
-            components.html(
-                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.1']}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
-                height=315, key='1'
-            )
+            update_video(video_placeholder, tutorial_links['Lesson 3.1'])
         if st.button('Lesson 3.2'):
-            components.html(
-                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.2']}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
-                height=315, key='2'
-            )
+            update_video(video_placeholder, tutorial_links['Lesson 3.2'])
     with col2:
         if st.button('Lesson 3.3'):
-            components.html(
-                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.3']}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
-                height=315, key='3'
-            )
+            update_video(video_placeholder, tutorial_links['Lesson 3.3'])
         if st.button('Lesson 3.4'):
-            components.html(
-                f"<iframe width='560' height='315' src='{tutorial_links['Lesson 3.4']}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
-                height=315, key='4'
-            )
+            update_video(video_placeholder, tutorial_links['Lesson 3.4'])
+
+def update_video(placeholder, video_url):
+    """Update the video in the placeholder with a new video URL."""
+    placeholder.html(
+        f"<iframe width='560' height='315' src='{video_url}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
+        height=315
+    )
 
 if __name__ == "__main__":
     main()
