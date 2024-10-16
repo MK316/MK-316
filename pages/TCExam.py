4a41c073e8f305af8dd25b54a44311606c41dad9
MK <99416359+MK316@users.noreply.github.com>
2024-10-16 22:08:46 +0900

    Update TCExam.py

--- pages/TCExam.py
+++ pages/TCExam.py
@@ -15,7 +15,7 @@ def tce_apps_page():
     with col1:
         st.image("images/TCE.png", width=100)
         if st.button('App 1: TCE exam search', key='1'):
-            st.markdown("🌀[App link](https://mk316-TCE.hf.space/): Searchable exam questions in phonetics & phonology", unsafe_allow_html=True)
+            st.markdown("🌀[App link](https://mk-316-TCE.hf.space/): Searchable exam questions in phonetics & phonology", unsafe_allow_html=True)
             st.markdown("2024.02.05")
 
 # Call the function to generate the page content first
