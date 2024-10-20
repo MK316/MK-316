854987b2c8b2e2635318d2b27755325dac2be0c7
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 05:31:30 +0900

    Update Class_is_ON_🚨.py

--- pages/Class_is_ON_🚨.py
+++ pages/Class_is_ON_🚨.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import webbrowser
 
 # Define the correct passcode
 correct_passcode = "1234"
-redirect_url = "https://huggingface.co/spaces/MK-316/mytimer"  # This URL should be the page you want to open upon successful login
+redirect_url = "https://coding4et.streamlit.app/Classapp"  # This URL should be the page you want to open upon successful login
 
 def show_passcode_input():
     st.title("Passcode Required")
@@ -12,8 +11,8 @@ def show_passcode_input():
     
     if submit_button:
         if input_passcode == correct_passcode:
-            # Open a new page if the passcode is correct
-            webbrowser.open_new_tab(redirect_url)
+            # Mark the session as authenticated
+            st.session_state['authenticated'] = True
         else:
             st.error("You don't seem to have permission!")
 
@@ -23,5 +22,6 @@ if 'authenticated' not in st.session_state:
 if not st.session_state['authenticated']:
     show_passcode_input()
 else:
-    # Optionally handle other content, but normally this would not execute as we redirect on success
-    st.write("You are already authenticated!")
+    # Display the link to the page when the passcode is correct
+    st.success("Access Granted")
+    st.markdown(f"[Proceed to the application]({redirect_url})", unsafe_allow_html=True)
