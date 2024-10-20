962114e4b16e7b3e0fe200cd946de39d5a84b2ea
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 05:52:31 +0900

    Update Apps:_⚪_Play_Sound.py

--- pages/Apps:_⚪_Play_Sound.py
+++ pages/Apps:_⚪_Play_Sound.py
@@ -4,13 +4,13 @@ def phonetics_apps_page():
     st.title('🐾 Play sound Apps')
     st.write('Applications used to teach Phonetics.')
 
+    # Describing the audio-related applications
     st.markdown("""
     Here is a selection of audio-related applications specifically designed to enhance phonetics learning. These tools cater to various needs, such as playing audio files, converting file formats, and utilizing Text-to-Speech technology. They offer interactive exercises to improve pronunciation, develop listening skills, and heighten phonetic awareness, making them invaluable resources for learners and educators alike.
     """)
 
-
-    # Using tabs instead of buttons
-    tab1, tab2 = st.tabs(["Play audio", "MP3-to-wav"])
+    # Using tabs for different apps
+    tab1, tab2, tab3 = st.tabs(["Play audio", "MP3-to-wav", "Multi-TTS"])
 
     with tab1:
         st.image("images/button03.png", width=100)
@@ -21,6 +21,11 @@ def phonetics_apps_page():
         st.image("images/button03.png", width=100)
         st.markdown("🌀 [App link](https://mk-316-mp3towav.hf.space/): Convert mp3 to wav file", unsafe_allow_html=True)
         st.markdown("2024.10.15")
+
+    with tab3:
+        st.image("images/button03.png", width=100)
+        st.markdown("🌀 [App link](https://MK-316-MultiTTS.hf.space): Multi-Text-to-Speech application", unsafe_allow_html=True)
+        st.markdown("Access the latest in Text-to-Speech technology.")
 
 phonetics_apps_page()
 
