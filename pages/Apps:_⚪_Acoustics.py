e6e46c3a542db26f218b9d807dd6f737649aeb1c
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 06:13:26 +0900

    Update Apps:_⚪_Acoustics.py

--- pages/Apps:_⚪_Acoustics.py
+++ pages/Apps:_⚪_Acoustics.py
@@ -2,11 +2,13 @@ import streamlit as st
 import numpy as np
 from scipy.io.wavfile import write
 from io import BytesIO
+import matplotlib.pyplot as plt
 
-def generate_tone(frequency, duration=1, sample_rate=44100):
+def generate_tone(frequency, duration=1, sample_rate=44100, amplitude=0.3):
+    """Generate a pure tone based on the frequency."""
     t = np.linspace(0, duration, int(sample_rate * duration), False)
-    tone = 0.5 * np.sin(2 * np.pi * frequency * t)
-    return np.int16(tone / np.max(np.abs(tone)) * 32767)
+    tone = amplitude * np.sin(2 * np.pi * frequency * t)  # Adjust amplitude here
+    return np.int16(tone / np.max(np.abs(tone)) * 32767), t, tone
 
 def main():
     st.title('Acoustics')
@@ -21,13 +23,20 @@ def main():
         generate_button = st.button('Generate Tone')
 
         if generate_button:
-            # Generate and display the tone
-            data = generate_tone(freq_input)
+            data, t, waveform = generate_tone(freq_input)
             # Write to a buffer
             buffer = BytesIO()
             write(buffer, 44100, data)
             buffer.seek(0)
             st.audio(buffer, format='audio/wav', start_time=0)
+
+            # Plotting the waveform
+            fig, ax = plt.subplots()
+            ax.plot(t, waveform)
+            ax.set_title(f"Waveform of the Generated Tone at {freq_input} Hz")
+            ax.set_xlabel('Time [s]')
+            ax.set_ylabel('Amplitude')
+            st.pyplot(fig)
 
     with tabs[2]:
         st.write("Details for Tab 3 will go here.")
