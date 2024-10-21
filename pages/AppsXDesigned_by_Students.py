21a6e5fc159d8085eb94b1cc7a5199c409add766
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:57:13 +0900

    Update AppsXDesigned_by_Students.py

--- pages/AppsXDesigned_by_Students.py
+++ pages/AppsXDesigned_by_Students.py
@@ -5,10 +5,10 @@ def main():
     st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way! (To be updated)")
 
     # Path to the image
-    image_path = 'https://github.com/MK316/MK-316/raw/main/images/octocat-2.png'
+    image_path = 'https://github.com/MK316/MK-316/raw/main/images/octocat-2-line.png'
 
     # Display the image
-    st.image(image_path, caption='Teaching is the best way of learning.', width=200)
+    st.image(image_path, caption='"The mediocre teacher tells. The good teacher explains. The superior teacher demonstrates. The great teacher inspires."', width=600)
 
 if __name__ == "__main__":
     main()
