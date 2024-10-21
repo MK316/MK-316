a160fd92bfd4528a36931e7c973e328ef1882408
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:31:42 +0900

    Update AppsXDesigned_by_Students.py

--- pages/AppsXDesigned_by_Students.py
+++ pages/AppsXDesigned_by_Students.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("🐾 Innovative Language Tools by Future Educators")
+st.title("🐾 Language Tools X Future Educators")
 
-st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way!")
+st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way! (To be updated)")
