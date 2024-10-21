9e085f4518b511e5cf29654317e8dac1e298a0ac
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:48:45 +0900

    Update AppsXDesigned_by_Students.py

--- pages/AppsXDesigned_by_Students.py
+++ pages/AppsXDesigned_by_Students.py
@@ -5,7 +5,7 @@ def main():
     st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way! (To be updated)")
 
     # Path to the image
-    image_path = 'data/octocat-2.png'
+    image_path = 'https://github.com/MK316/MK-316/raw/main/images/octocat-2.png'
 
     # Display the image
     st.image(image_path, caption='Sample Image', use_column_width=True)
