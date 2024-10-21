de107566d40c02bebc984514f7e38ee00d792c43
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:24:36 +0900

    Update and rename AppsXStudent_Made.py to AppsXDesigned_by_Students.py

---
+++ pages/AppsXDesigned_by_Students.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("🐾 Innovative Language Tools by Future Educators")
+
+st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way!")
--- pages/AppsXStudent_Made.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("🐾 Student designed language applications")
-st.write("This page will host applications designed by prospect English teachers. Coming soon!")
