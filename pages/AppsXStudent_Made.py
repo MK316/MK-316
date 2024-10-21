701642fea1737ed2becd70bba6e3164cf7a3c430
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:22:23 +0900

    Update AppsXStudent_Made.py

--- pages/AppsXStudent_Made.py
+++ pages/AppsXStudent_Made.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Student designed language applications")
+st.title("🐾 Student designed language applications")
 st.write("This page will host applications designed by prospect English teachers. Coming soon!")
