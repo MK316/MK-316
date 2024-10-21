2a1eb9b332aff5876a290c8f20c5ad12e8cad75b
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 15:47:42 +0900

    Update AppsXDesigned_by_Students.py

--- pages/AppsXDesigned_by_Students.py
+++ pages/AppsXDesigned_by_Students.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
-st.title("🐾 Language Tools X Future Educators")
+def main():
+    st.title("🐾 Language Tools X Future Educators")
+    st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way! (To be updated)")
 
-st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way! (To be updated)")
+    # Path to the image
+    image_path = 'data/octocat-2.png'
+
+    # Display the image
+    st.image(image_path, caption='Sample Image', use_column_width=True)
+
+if __name__ == "__main__":
+    main()
