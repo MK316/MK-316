8f7e095b6274ba86165439aa35438cacd0c378ef
MK <99416359+MK316@users.noreply.github.com>
2024-10-21 16:16:10 +0900

    Update AppsXDesigned_by_Students.py

--- pages/AppsXDesigned_by_Students.py
+++ pages/AppsXDesigned_by_Students.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def main():
-    st.title("🐾 Language Tools X Future Educators")
+    st.title("🐾 Tools X Future Educators")
     st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way!")
     st.markdown("<br>", unsafe_allow_html=True)
-    st.write("(To be updated)")
+    st.markdown("_(To be updated)_")
     st.markdown("<br><br><br>", unsafe_allow_html=True)
     
     # Path to the image
