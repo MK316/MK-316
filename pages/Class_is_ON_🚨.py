63f964baf8bde77280a91171a96d86a4cc6b5dbc
MK <99416359+MK316@users.noreply.github.com>
2024-11-02 23:09:58 +0900

    Update Class_is_ON_🚨.py

--- pages/Class_is_ON_🚨.py
+++ pages/Class_is_ON_🚨.py
@@ -91,7 +91,7 @@ with tab1:
             # Display the Midterm score if the passcode is found
             if not filtered_data.empty:
                 midterm_score = filtered_data['Midterm'].values[0]
-                st.write(f"Your Midterm score is: {midterm_score}")
+                st.write(f"Your Midterm score is: {midterm_score} out of 75 points")
             else:
                 st.write("Invalid Passcode for Midterm Score. Please try again.")
         
