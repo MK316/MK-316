32dac820078e9c8d0ef5f26df244e477aad8fdd4
MK <99416359+MK316@users.noreply.github.com>
2024-11-05 23:57:41 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -3,7 +3,7 @@ import streamlit as st  # Corrected the import alias to 'st' from 'sr'
 st.markdown("### About me...")
 st.markdown("![Profile Image](https://github.com/MK316/MK-316/raw/main/images/profile-small.jpg)")  # Correct markdown syntax for images
 st.markdown("\n\n")
-st.markdown("- Linguist & educator")
+st.markdown("- Linguist (Phonetics/Phonology) & educator")
 st.markdown("- Github ID: MK316")
 st.markdown("- Affiliation: Dept. of English Education, GNU ⛺ [Goto](https://englishedu.gnu.ac.kr)")
 st.markdown("- Archives (before 10. 14. 2024) - My digital classroom: [mrkim21.github.io](https://mrkim21.github.io)")
