7e12eabd2104f6abd4d7933a525ab4ca864f1547
MK <99416359+MK316@users.noreply.github.com>
2024-11-06 00:31:51 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,5 +1,3 @@
-import streamlit as st  # Corrected the import alias to 'st' from 'sr'
-
 import streamlit as st
 import requests
 
@@ -21,34 +19,29 @@ def main():
 
     # Fall 2024 content
     with tabs[0]:
-
         st.markdown("### About me...")
-        st.markdown("![Profile Image](https://github.com/MK316/MK-316/raw/main/images/profile-small.jpg)")  # Correct markdown syntax for images
-        st.caption("I teach phonetics and phonology to English education majors, focusing on advancing digital literacy for today’s evolving classrooms. My work emphasizes student-centered, technology-enhanced learning to equip future teachers with interactive, engaging methods that minimize reliance on commercial applications. Since February 2022, I have been learning foundational Python coding to enhance instructional approaches and am dedicated to developing professional courses for in-service teachers interested in integrating digital tools. I am also exploring coding-based language learning app development. Welcome aborad!")
+        st.markdown("![Profile Image](https://github.com/MK316/MK-316/raw/main/images/profile-small.jpg)")
+        st.caption("I teach phonetics and phonology to English education majors, focusing on advancing digital literacy for today’s evolving classrooms. My work emphasizes student-centered, technology-enhanced learning to equip future teachers with interactive, engaging methods that minimize reliance on commercial applications. Since February 2022, I have been learning foundational Python coding to enhance instructional approaches and am dedicated to developing professional courses for in-service teachers interested in integrating digital tools. I am also exploring coding-based language learning app development. Welcome aboard!")
         st.markdown("\n\n")
         st.markdown("- Linguist (Phonetics/Phonology) & educator")
         st.markdown("- Github ID: MK316")
         st.markdown("- Affiliation: Dept. of English Education, GNU ⛺ [Goto](https://englishedu.gnu.ac.kr)")
         st.markdown("- Archives (before 10. 14. 2024) - My digital classroom: [mrkim21.github.io](https://mrkim21.github.io)")
 
-# URL to the raw image on GitHub
+        # URL to the raw image on GitHub
         image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
-# Display the image
+        # Display the image
         st.image(image_url, caption="\"Be curious, not judgmental.\" — Walt Whitman", use_column_width=True)
 
     # Publications
     with tabs[1]:
-    #    st.subheader("Publications")
         spring_url = 'https://github.com/MK316/MK316.github.io/blob/main/res/publications.md'
         spring_content = fetch_github_readme(spring_url)
         st.markdown(spring_content, unsafe_allow_html=True)
 
     # Academic services
     with tabs[2]:
-    #    st.subheader("Academic services")
-        # Placeholder URL for additional content if needed
-        # Uncomment and update the URL if you have content for this tab
-         url = 'https://github.com/MK316/MK-316/blob/pages/academicservice.md'
+        url = 'https://github.com/MK316/MK-316/blob/main/pages/academicservice.md'
         additional_content = fetch_github_readme(url)
         st.markdown(additional_content, unsafe_allow_html=True)
 
@@ -61,12 +54,5 @@ def main():
         # additional_content = fetch_github_readme(additional_url)
         # st.markdown(additional_content, unsafe_allow_html=True)
 
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
