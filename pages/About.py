75205f2953ddfe0b5c8d4e4f308cc8e00eddd746
MK <99416359+MK316@users.noreply.github.com>
2024-11-06 00:36:34 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -34,6 +34,10 @@ def main():
 
     # Publications
     with tabs[1]:
+        # URL to the raw image on GitHub
+        image_url = "https://github.com/MK316/MK316.github.io/blob/main/images/KeywordCloud_231129.png"
+        # Display the image
+        st.image(image_url, caption="Research keywords", use_column_width=True)
         spring_url = 'https://github.com/MK316/MK316.github.io/blob/main/res/publications.md'
         spring_content = fetch_github_readme(spring_url)
         st.markdown(spring_content, unsafe_allow_html=True)
