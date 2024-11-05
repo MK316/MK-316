5f975eb22967cfe31b78ed7c0e7abdcce1f32e5b
MK <99416359+MK316@users.noreply.github.com>
2024-11-05 18:17:14 +0900

    Update Apps:_⚪_Phonetics-Vowels.py

--- pages/Apps:_⚪_Phonetics-Vowels.py
+++ pages/Apps:_⚪_Phonetics-Vowels.py
@@ -8,12 +8,12 @@ tab1, tab2 = st.tabs(["Vowel Chart", "V-description app"])
 
 # Define the content of each tab
 with tab1:
-    st.header("Vowel Chart")
+    st.header("Vowel Chart with Formants (F1 and F2)")
     st.markdown("This tool provides a vowel chart for phonetics practice.")
     
     # Create a link styled as a button that opens the URL in a new tab
     vowel_chart_url = "https://vowelchart.streamlit.app"
-    st.markdown(f'<a href="{vowel_chart_url}" target="_blank" style="display: inline-block; text-decoration: none; background-color: #4CAF50; color: white; padding: 10px 20px; border-radius: 5px;">Open Vowel Charting</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="{vowel_chart_url}" target="_blank" style="display: inline-block; text-decoration: none; background-color: #FF9933; color: white; padding: 10px 20px; border-radius: 5px;">Open Vowel Charting App</a>', unsafe_allow_html=True)
 
     st.caption("This tool provides a vowel chart based on formants (F1, F2).")
 
