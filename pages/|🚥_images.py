dd164209e57edcf8d418f2b0bc33d4e7b9e781bd
MK <99416359+MK316@users.noreply.github.com>
2024-11-07 10:42:44 +0900

    Update |🚥_images.py

--- pages/|🚥_images.py
+++ pages/|🚥_images.py
@@ -4,13 +4,13 @@ tab1, tab2, tab3 = st.tabs(["2024 Gallery", "gallery 2", "gallery 3"])
 
 with tab1:
     st.header("2024 Memories")
-    st.image("https://github.com/MK316/MK-316/raw/main/images/2410a.png", width=200)
+    st.image("https://github.com/MK316/MK-316/raw/main/images/2410a.png", width=300)
     st.write("01")
-    st.image("https://github.com/MK316/MK-316/raw/main/images/2410b.png", width=200)
+    st.image("https://github.com/MK316/MK-316/raw/main/images/2410b.png", width=300)
     st.write("02")
-    st.image("https://github.com/MK316/MK-316/raw/main/images/2410c.png", width=200)
+    st.image("https://github.com/MK316/MK-316/raw/main/images/2410c.png", width=300)
     st.write("03")
-    st.image("https://github.com/MK316/MK-316/raw/main/images/2409a.png", width=200)
+    st.image("https://github.com/MK316/MK-316/raw/main/images/2409a.png", width=300)
 with tab2:
     st.header("A dog")
     st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
