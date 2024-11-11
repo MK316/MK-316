41c94efd6d596f6bd06f3821824ab6f5818eafcc
MK <99416359+MK316@users.noreply.github.com>
2024-11-12 02:01:47 +0900

    Update Apps_X_Designed_by_Teachers.py

--- pages/Apps_X_Designed_by_Teachers.py
+++ pages/Apps_X_Designed_by_Teachers.py
@@ -1,15 +1,14 @@
 import streamlit as st
 
 def main():
-    st.title("💭 Tools X English Teachers")
-    st.write("Welcome to our curated showcase of language applications, thoughtfully created by active English teachers. These tools are developed with real classroom experiences in mind to enhance both learning and teaching of languages. Stay tuned—exciting new tools are on the way!")
+    st.title("🐾 Tools X Future Educators")
+    st.write("Welcome to our dynamic showcase of language applications, creatively designed by aspiring English teachers. These applications are crafted to enrich language learning and teaching experiences. Stay tuned—exciting developments are on the way!")
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("_(To be updated)_")
     st.markdown("<br><br><br>", unsafe_allow_html=True)
     
     # Path to the image
     image_path = 'https://github.com/MK316/MK-316/raw/main/images/octocat-2-line.png'
-
             
     # Display the image
     st.image(image_path, width=600)
@@ -22,6 +21,29 @@ def main():
         - <span style='font-style: italic;'>W. A. Ward</span>
     </div>
     """, unsafe_allow_html=True)
+
+    # Create multiple tabs for different sections
+    tab1, tab2, tab3, tab4 = st.tabs(["Tab 1: 1. Verb tense practice", "Tab 2: 2. Noun plurals", "Tab 3: TBA", "Tab 4: TBA"])
+
+    # Content for Tab 1
+    with tab1:
+        st.header("Practice verb froms Present - Past - Past Participle")
+        st.write("Team #1: This project aims to implement an application that allows practice of the three forms of basic verbs that appear in high school textbooks. (November 2024)")
+
+    # Content for Tab 2
+    with tab2:
+        st.header("Regular and irregular noun plural form practice")
+        st.write("Team #2: This project aims to implement an application that allows practice of plural forms of English nouns.")
+
+    # Content for Tab 3
+    with tab3:
+        st.header("Welcome to Tab 3")
+        st.write("Here's a detailed look at the tools developed by future educators.")
+
+    # Content for Tab 4
+    with tab4:
+        st.header("Welcome to Tab 4")
+        st.write("Get inspired by the stories and successes of those who've used these tools.")
 
 if __name__ == "__main__":
     main()
