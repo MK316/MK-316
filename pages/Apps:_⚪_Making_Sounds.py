4e5975879b8e2fae3de0cc7f204a6bf2ef1c45b5
MK <99416359+MK316@users.noreply.github.com>
2024-11-11 14:41:43 +0900

    Update Apps:_⚪_Making_Sounds.py

--- pages/Apps:_⚪_Making_Sounds.py
+++ pages/Apps:_⚪_Making_Sounds.py
@@ -102,7 +102,7 @@ def phonetics_apps_page():
     with tab4:
         st.header("Generate your own melody")
         st.caption("Using this app, the user can generate a downloadable audio file.")
-        st.caption("The sequence "do, re, mi, fa..." is called the solfege system, or solfège, a method used to teach pitch and sight singing in music. Each syllable corresponds to a note on a musical scale, allowing for easy vocalization and learning of musical notation.")
+        st.caption("The sequence 'do, re, mi, fa...' is called the solfege system, or solfège, a method used to teach pitch and sight singing in music. Each syllable corresponds to a note on a musical scale, allowing for easy vocalization and learning of musical notation.")
         appurl="https://melody-play.streamlit.app/"
         st.markdown(f'<a href="{url}" target="_blank">Click here to open the page in a new tab</a>', unsafe_allow_html=True)
 # Run the phonetics apps page
