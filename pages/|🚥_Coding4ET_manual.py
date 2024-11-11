ebfd5824cba0358ada922593d455cd2fe2423e42
MK <99416359+MK316@users.noreply.github.com>
2024-11-11 14:18:10 +0900

    Update |🚥_Coding4ET_manual.py

--- pages/|🚥_Coding4ET_manual.py
+++ pages/|🚥_Coding4ET_manual.py
@@ -51,10 +51,11 @@ def main():
 
     with tabs[7]:
         st.subheader("Practical Python Manual by David Beazley")
+        st.caption("This manual is suitable for those who want to further study the basics of Python coding.")
         st.caption("Github source: https://github.com/dabeaz-course/practical-python.")
         st.write("Click below to go to the manual page.")
         readme_url = 'https://wikidocs.net/book/4606'
-        link_text = f"View the manual [here]({readme_url})"
+        link_text = f"📗 View the manual [here]({readme_url})"
         st.markdown(link_text, unsafe_allow_html=True)
     
 def handle_lesson_3_videos():
