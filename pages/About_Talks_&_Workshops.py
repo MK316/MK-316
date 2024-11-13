e04b941dc6fd43e0521ad016db5bc0fb8dd92a7b
MK <99416359+MK316@users.noreply.github.com>
2024-11-13 19:01:45 +0900

    Update About_Talks_&_Workshops.py

--- pages/About_Talks_&_Workshops.py
+++ pages/About_Talks_&_Workshops.py
@@ -29,7 +29,8 @@ def main():
     # Spring 2024 content
     with tabs[1]:
         st.subheader("2024 제1회 경상 디지털교육 나눔 한마당")
-        st.markdown("🌀 [Go to my presentation page]('https://241214.streamlit.app/')")
+        st.markdown('<a href="https://241214.streamlit.app/" target="_blank">🌀 Go to my presentation page</a>', unsafe_allow_html=True)
+
         st.write("AIED Symposium Session I: 현장교사를 위한 앱 개발 및 응용 사례")
         st.markdown("+ Organized by: G-DEAL, GNU \n+ Hosted by: Gyeongsang National University \n+ Date & Time: 13:00~14:30, Dec.14 (Sat), 2024 \n+ Location: GNU Convention Center")
         st.caption("More information to be updated.")
