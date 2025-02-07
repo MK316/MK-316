3d8dcc198abd5e868d65927840f79a8083cdaa92
MK <99416359+MK316@users.noreply.github.com>
2025-02-07 14:06:25 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ st.markdown("### _Welcome to my application gallery_")
 st.markdown("━ A space to discover innovative language learning applications powered by Streamlit! The gallery is constantly updated. You can also explore my previous digital classroom to check out customized applications on my Github at [mrkim21.github.io](https://mrkim21.github.io/appfolder/appmain.html).")
 
 st.markdown("""
-I am learning to code to develop customized, **_customized_**, **_learner-centered_** language apps that align with the specific needs of my students. My aim is to create **_simple, interactive_** tools that enhance the effectiveness and engagement of language learning. I look forward to exploring innovative and impactful teaching methods together.
+I am learning to code to develop **_customized_**, **_interactive_**, **_learner-centered_** language apps that align with the specific needs of my students. My aim is to create **_simple, interactive_** tools that enhance the effectiveness and engagement of language learning. I look forward to exploring innovative and impactful teaching methods together.
 """)
 st.caption("Since Oct. 14, 2024")
 st.caption("Laste updated: Feb.07, 2025")
