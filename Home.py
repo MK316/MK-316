eb361ef3501abfd03ba2de4e8dc5df7104fb7ebf
MK <99416359+MK316@users.noreply.github.com>
2025-02-07 14:05:25 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,13 +4,13 @@ st.image("https://github.com/MK316/MK-316/raw/main/images/mk316.png", caption=" 
 st.markdown("### _Welcome to my application gallery_")
 
 # Additional content
-st.markdown("━ A space to discover innovative language learning applications powered by Streamlit! The gallery is currently being prepared. Meanwhile, you can visit my digital classroom at [mrkim21.github.io](https://mrkim21.github.io/appfolder/appmain.html).")
+st.markdown("━ A space to discover innovative language learning applications powered by Streamlit! The gallery is constantly updated. You can also explore my previous digital classroom to check out customized applications on my Github at [mrkim21.github.io](https://mrkim21.github.io/appfolder/appmain.html).")
 
 st.markdown("""
 I am learning to code to develop customized, **_customized_**, **_learner-centered_** language apps that align with the specific needs of my students. My aim is to create **_simple, interactive_** tools that enhance the effectiveness and engagement of language learning. I look forward to exploring innovative and impactful teaching methods together.
 """)
 st.caption("Since Oct. 14, 2024")
-st.caption("Laste updated: Nov.14, 2024")
+st.caption("Laste updated: Feb.07, 2025")
 # URL to the raw image on GitHub
 image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
 # Display the image
