35e30e21a3fc3fc3177abac525e7dd3a87b3f333
MK <99416359+MK316@users.noreply.github.com>
2025-02-10 12:30:49 +0900

    Update Apps:_⚪_English_learning.py

--- pages/Apps:_⚪_English_learning.py
+++ pages/Apps:_⚪_English_learning.py
@@ -58,4 +58,4 @@ elearning_apps_page()
 # URL to the raw image on GitHub
 image_url = "https://github.com/MK316/MK-316/raw/main/images/bg2.png"
 # Display the image
-st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_column_width=True)
+st.image(image_url, caption="\"Every student can learn, just not on the same day, or the same way.\" – George Evans", use_container_width=True)
