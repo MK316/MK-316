feb89ab95bb72658409ea81e0385d0f68f9ecb73
MK <99416359+MK316@users.noreply.github.com>
2025-03-13 00:54:48 +0900

    Update |🚥_Photobook.py

--- pages/|🚥_Photobook.py
+++ pages/|🚥_Photobook.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["2024 Gallery", "gallery 2", "gallery 3"])
+tab1, tab2, tab3 = st.tabs(["2024 Gallery", "2025 Gallery", "gallery 3"])
 
 with tab1:
     st.header("2024 Memories")
@@ -12,8 +12,8 @@ with tab1:
     st.write("03")
     st.image("https://github.com/MK316/MK-316/raw/main/images/2409a.png", width=300)
 with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.header("2025 Memories")
+    st.image("https://github.com/MK316/MK-316/blob/main/images/KSW20250312.png", width=400)
 with tab3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
